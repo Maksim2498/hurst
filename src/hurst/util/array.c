810c1c9d34c0d9d9e9b3f202e532fdc1604e3071
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-11-29 03:46:07 +0300

    Added more array functions

--- src/hurst/util/array.c
+++ src/hurst/util/array.c
@@ -1,6 +1,7 @@
 #include "array.h"
 
 #include <assert.h>
+#include <math.h>
 
 bool allNotNull(const void* const* array, size_t len) {
     if (!array)
@@ -11,4 +12,36 @@ bool allNotNull(const void* const* array, size_t len) {
             return false;
 
     return true;
+}
+
+double evalStd(const double* array, size_t len) {
+    const double avg = evalAvg(array, len);
+
+    double sum = 0;
+
+    for (size_t i = 0; i < len; ++i) {
+        const double diff = array[i] - avg;
+
+        sum += diff * diff;
+    }
+
+    return sqrt(sum / len);
+}
+
+double evalAvg(const double* array, size_t len) {
+    return evalSum(array, len) / len;
+}
+
+double evalSum(const double* array, size_t len) {
+    assert(array);
+
+    if (!len)
+        return 0;
+
+    double sum = array[0];
+
+    for (size_t i = 1; i < len; ++i)
+        sum += array[i];
+
+    return sum;
 }
--- src/hurst/util/array.h
+++ src/hurst/util/array.h
@@ -3,7 +3,12 @@
 
 #include <stddef.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 bool allNotNull(const void* const* array, size_t len);
+
+double evalStd(const double* array, size_t len);
+double evalAvg(const double* array, size_t len);
+double evalSum(const double* array, size_t len);
 
 #endif
