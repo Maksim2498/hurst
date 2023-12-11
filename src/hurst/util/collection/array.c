b803559c8a2c100a103519cea782bb47f79412cd
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2023-12-12 02:31:49 +0300

    Implemented sortArray function

--- src/hurst/util/collection/array.c
+++ src/hurst/util/collection/array.c
@@ -1,7 +1,12 @@
 #include "array.h"
 
 #include <assert.h>
+#include <string.h>
 #include <math.h>
+
+
+static char* mergeSortArray_(char* first, char* second, size_t len, size_t itemSize, Cmp cmp);
+
 
 bool allNotNull(const void* const* array, size_t len) {
     if (!array)
@@ -87,10 +92,66 @@ double evalDArraySum(const double* array, size_t len) {
 }
 
 void sortArray(void* array, void* buffer, size_t len, size_t itemSize, Cmp cmp) {
-    assert(array && buffer && itemSize && cmp);
+    void* last = mergeSortArray_(array, buffer, len, itemSize, cmp);
 
-    if (cmp)
-        len = -len;
+    if (last != array)
+        memcpy(array, buffer, len * itemSize);
+}
 
-    // TODO
+char* mergeSortArray_(char* first, char* second, size_t len, size_t itemSize, Cmp cmp) {
+    assert(first && second && itemSize && cmp);
+
+    if (len <= 1)
+        return first;
+
+    const size_t leftLen   = len / 2;
+    const size_t rightLen  = len - leftLen;
+    const size_t leftSize  = leftLen  * itemSize;
+    const size_t rightSize = rightLen * itemSize;
+
+    char* left     = mergeSortArray_(first,            second,            leftLen,  itemSize, cmp);
+    char* right    = mergeSortArray_(first + leftSize, second + leftSize, rightLen, itemSize, cmp);
+    char* target   = left == first ? second : first;
+
+    char* leftEnd  = left  + leftSize;
+    char* rightEnd = right + rightSize;
+
+    char* leftIt   = left;
+    char* rightIt  = right;
+    char* targetIt = target;
+
+    #define PUSH_LEFT()                         \
+        do {                                    \
+            memcpy(targetIt, leftIt, itemSize); \
+            leftIt += itemSize;                 \
+        } while (0)
+
+    #define PUSH_RIGHT()                         \
+        do {                                     \
+            memcpy(targetIt, rightIt, itemSize); \
+            rightIt += itemSize;                 \
+        } while (0)
+
+    for (size_t i = 0; i < len; ++i, targetIt += itemSize) {
+        if (leftIt < leftEnd && rightIt < rightEnd) {
+            if (cmp(leftIt, rightIt) <= 0)
+                PUSH_LEFT();
+            else
+                PUSH_RIGHT();
+
+            continue;
+        }
+
+        if (leftIt < leftEnd) {
+            PUSH_LEFT();
+            continue;
+        }
+
+        PUSH_RIGHT();
+    }
+
+    #undef PUSH_LEFT
+    #undef PUSH_RIGHT
+
+    return target;
 }
